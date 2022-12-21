2dd6862b8c2c02311d452714c3774ca1c7881a20
草鞋没号 <308487730@qq.com>
2022-12-21 14:31:26 +0800

    refactor!: redefine export members

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,9 @@
-import type { FSWatcher } from 'chokidar'
-import {
+export {
   type Configuration,
   type ResolvedConfig,
   resolveConfig,
 } from './config'
-import { normalizePath, logger } from './utils'
-import {
-  type BuildResult,
-  build
-} from './build'
-import { watch } from './watch'
-
-// public export
-export {
-  type Configuration,
-  type ResolvedConfig,
-  type FSWatcher,
-  type BuildResult,
-  resolveConfig,
-  build,
-  watch,
-
-  // utils
-  normalizePath,
-  logger,
-}
-
-export default async function notbundle(config: Configuration): Promise<FSWatcher | BuildResult[]> {
-  return config.watch ? (await watch(config))! : build(config)
-}
+export { type BuildResult, build } from './build'
+export { type FSWatcher } from 'chokidar'
+export { watch } from './watch'
+export { normalizePath } from './utils'
